90804f31fed7ade5373224ef987555371c0465ca
chenfan <chenfan@chenfandeMacBook-Pro.local>
2017-02-11 14:08:25 +0800

    Revert "recommit"
    This reverts commit 44a7ab1196aa43290f511ece9a8d70afbd807eed.

--- _site/pre-build.sh
+++
@@ -1,9 +0,0 @@
-rm -rf categories
-rm -f categorycount.json
-jekyll build 
-cp -r _site/categories .
-cp -r _site/categorycount.json .
-
-if [ "$1" = "test" ];then
-    jekyll serve  --force_polling
-fi
