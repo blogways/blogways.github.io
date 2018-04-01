62b52ff9a50815c9be28d0948a8787016f9e8dbf
林明星 <1456132848@qq.com>
2018-04-02 00:45:48 +0800

    修复小屏幕下拉框问题

--- js/index.js
+++ js/index.js
@@ -72,8 +72,6 @@ $(function() {
     },function(){
         $(this).removeClass('open');
     })
-    //子元素滚动，父元素不滚动
-    $(".wheel").scrollUnique();
 });
 //传入需要在首页展示的博客数量，然后展示。
 function showBlog(num) {
@@ -127,26 +125,4 @@ function by(articles){
       }
     }
 };
-//子元素滚动，父元素不滚动的函数
-$.fn.scrollUnique = function() {
-    return $(this).each(function() {
-        var eventType = 'mousewheel';
-        // 火狐是DOMMouseScroll事件
-        if (document.mozHidden !== undefined) {
-            eventType = 'DOMMouseScroll';
-        }
-        $(this).on(eventType, function(event) {
-            // 一些数据
-            var scrollTop = this.scrollTop,
-                scrollHeight = this.scrollHeight,
-                height = this.clientHeight;
-            var delta = (event.originalEvent.wheelDelta) ? event.originalEvent.wheelDelta : -(event.originalEvent.detail || 0);        
-            if ((delta > 0 && scrollTop <= delta) || (delta < 0 && scrollHeight - height - scrollTop <= -1 * delta)) {
-                // IE浏览器下滚动会跨越边界直接影响父级滚动，因此，临界时候手动边界滚动定位
-                this.scrollTop = delta > 0? 0: scrollHeight;
-                // 向上滚 || 向下滚
-                event.preventDefault();
-            }        
-        });
-    }); 
-};
+
