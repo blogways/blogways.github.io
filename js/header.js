fc5e4a189c1ae3a6a0afc640b8d8dc2af30acf05
林明星 <1456132848@qq.com>
2018-04-01 19:31:51 +0800

    小屏幕导航栏下拉框问题修复

--- js/header.js
+++ js/header.js
@@ -23,19 +23,27 @@ $(function(){
 		}
 	})
 
-	$('#cate').css('height', window.screen.availHeight/2);
-	$('#us').css('height', window.screen.availHeight/2);
+	if($(window).width() > 1200){
+		$('#cate').css('height', window.screen.availHeight/2);
+		$('#us').css('height', window.screen.availHeight/2);
+	}else{
+		$('#cate').css('height', 'auto');
+		$('#us').css('height', 'auto');
+	}
 	window.onresize = function(){
 		if($(window).width() > 1200){
-			$('.navbar-nav').css('display', 'block');
-			
+			$('#cate').css('height', window.screen.availHeight/2);
+			$('#us').css('height', window.screen.availHeight/2);
 		}
 		if($(window).width() <= 1200 && $(window).width() > 750){
 			$('.navbar-nav').css('display', 'none');
-			
+			$('#cate').css('height', 'auto');
+			$('#us').css('height', 'auto');
 		}	
 		if($(window).width() <= 750){
 			$('.navbar-nav').css('display', 'block');
+			$('#cate').css('height', 'auto');
+			$('#us').css('height', 'auto');
 		}	
 	}
 	$(".navbar-toggle").click(function(){
